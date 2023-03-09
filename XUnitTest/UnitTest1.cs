127329913e954013fd598832f783061a714481ca
Chris Davey <chris.davey@allpay.net>
2023-03-09 09:59:33 +0000

    init

---
+++ XUnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using ClassLibrary;
+using Moq;
+using Xunit;
+
+namespace XUnitTest
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var imp = new Mock<IInterface>();
+        }
+    }
+}
